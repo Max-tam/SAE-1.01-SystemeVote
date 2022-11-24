f0aa240e419165da73f9211acd1d8e27c5fe99ac
Maxime TAMARIN <115541159+Max-tam@users.noreply.github.com>
2022-11-24 21:07:51 +0100

    version finale (manque explication //)
    teste de verification retiré.
    modification de la méthode systemeMajoritaire() au niveau du renvoi

--- Majoritaire.cpp
+++ Majoritaire.cpp
@@ -1,6 +1,6 @@
 /*
 auteur: Maxime TAMARIN
-dernière modification: 24 /11/2022
+dernière modification: 24/11/2022 21:04
 */
 
 #include <iostream>
@@ -14,15 +14,9 @@ vector <int> vote(const vector <string> & jeuxCandidat,const vector <string> & v
     string candidatVote; // correspond au numero de placement dans la liste du jeux choisi (au départ il est sous forme de string
     vector <int> resultat; // tableau contenant les résulats ou le resultat resultat[i] se rapporte au candidat candidat[i]
     resultat.resize(jeuxCandidat.size()); // initialisation de la longueur à la même que cel du tableau candidat
-    cout << "\n\n---| choix du jeux |---\n\n";
-    for (size_t i = 0; i < jeuxCandidat.size(); ++i)
-    {
-        cout << jeuxCandidat[i] << " : " << i << endl; // affichage du jeux candidat et de son indice (place) attribué
-    }
     while (true) // boucle infini
     {
         if ( indiceVotant >= votant.size()) break; // condition de sortie de la boucle infini
-        cout << "\nparmi les jeux suivant quel est le meilleur ?\n\n"<< votant[indiceVotant]<< " -> ";
         getline(cin,candidatVote);// choix de l'utilisateur du jeux par son indice
         if (stoi(candidatVote) < jeuxCandidat.size()) // condition si l'indice correspond bien à la liste donné (pour éviter des erreurs de dépassement) (vote non pris en compte sinon)
         {
@@ -30,7 +24,7 @@ vector <int> vote(const vector <string> & jeuxCandidat,const vector <string> & v
         }
         ++indiceVotant;
     }
-    cout << "\n\n---| Resultat du vote |---\n\n";
+    cout << "\n---| Resultat du vote |---\n\n";
     for (size_t i = 0; i < jeuxCandidat.size() ; ++i)
     {
         cout << jeuxCandidat[i] << " : " << resultat[i] << endl; // affiche les jeux et le nombres de voies attribué à la fin du vote
@@ -53,7 +47,13 @@ string systemeMajoritaire(const vector <int> & resultat, const vector <string> &
         }
         else if(resultat[indiceGagnant] == resultat[i]) egalite = 1; // détecte si il y a une égalité
     }
-    if (egalite == 1) return "egalite"; // si il y a égalité
+    if (egalite == 1)
+    {
+        cout << "\nil n'y a pas de vainqueur" << endl;
+        return "egalite"; // si il y a égalité
+    }
+    else
+        cout << "\nLe gagnant est "<<gagnant << endl;
     return gagnant; // si il n'y en a pas
 }
 
@@ -73,7 +73,6 @@ int AnalyseEntree()
         getline(cin,prenom);
         vNomPrenom.resize(vNomPrenom.size()+1);
         vNomPrenom[vNomPrenom.size()-1] = nom + " " + prenom;
-        cout << vNomPrenom[vNomPrenom.size()-1] << endl;
     }
     vector <string> candidat = { "Counter strike", "Street Fighter II","Civilization VI","Mario Kart"}; // initialisation de la liste des jeux candidats
     for (int i = 0; i < nombreTeste; ++i) // suite à verifier (de ici)
@@ -84,23 +83,13 @@ int AnalyseEntree()
         {
             getline(cin,ligneFichier);
             if (stoi(ligneFichier) != resultatVote[j]) // stoi() convertion string --> int
-            {
-                cout << "teste faux" << endl;
                 return -1;
-            }
-            else
-               cout << "teste passé" << endl; // à ici
         }
         string nomGagnant;
         nomGagnant = systemeMajoritaire(resultatVote,candidat);
         getline(cin,ligneFichier);
         if (ligneFichier != nomGagnant)
-        {
-            cout << "gagnant faux";
             return -1;
-        }
-        else
-            cout << "gagnant juste";
     }
     return 1;
 }
